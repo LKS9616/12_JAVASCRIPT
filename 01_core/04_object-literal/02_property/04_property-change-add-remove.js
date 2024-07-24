53563ab7ffdc64459c2d02d208b2e89beb1698ff
lks9616 <lks609591@gmail.com>
2024-07-24 18:03:58 +0900

    커밋

---
+++ 01_core/04_object-literal/02_property/04_property-change-add-remove.js
@@ -0,0 +1,17 @@
+var dog = {
+    name : '구찌',
+};
+
+// 이미 존재하는 프로퍼티에 값을 할당하면 프로퍼티 값이 갱신된다.
+dog.name = '가린';
+// dog['name'] = '가린';
+console.log(dog);
+
+// 프로퍼티 동적 추가
+// 존재하지 않는 프로퍼티에 값을 할당하면 프로퍼티가 동적으로 생성되어 추가되고, 프로퍼티 값이 할당된다.
+dog.age = 3;
+console.log(dog);
+
+// 프로퍼티 삭제
+delete dog.age;
+console.log(dog);
